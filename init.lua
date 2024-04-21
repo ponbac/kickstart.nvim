d708b3a092187b2894da004462ab4021c6ef9817
ponbac <root@ponbac.xyz>
2024-04-21 17:03:04 +0200

    tw

--- init.lua
+++ init.lua
@@ -519,6 +519,8 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        tailwindcss = {},
+        eslint = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
